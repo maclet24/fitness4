10a33ae290bbf0ee790c21c453bd6c8ae23618a7
Ryan McKennon <Ryan McKennon>
2016-04-27 03:08:10 -0500

    working workout page

--- www/js/db.js
+++ www/js/db.js
@@ -5,18 +5,27 @@
         var day;
         var update_day_id;
         var added_value;
+        var monday = "Monday";
+        var tuesday = "Tuesday";
+        var wednesday = "Wednesday";
+        var thursday = "Thursday";
+        var friday = "Friday";
+        var saturday = "Saturday";
+        var sunday = "Sunday";
+        var totalLength;
+        var dayQuery;
 
         // Populate the database
         //
         function populateDB(tx) {
             tx.executeSql('CREATE TABLE IF NOT EXISTS EXERCISE (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, reps INTEGER, sets INTEGER, timer INTEGER,increment INTEGER)');
-            tx.executeSql('CREATE TABLE IF NOT EXISTS MONDAY (id INTEGER PRIMARY KEY AUTOINCREMENT, exercise TEXT NOT NULL, added INTEGER)');
-            tx.executeSql('CREATE TABLE IF NOT EXISTS TUESDAY (id INTEGER PRIMARY KEY AUTOINCREMENT,exercise TEXT NOT NULL, added INTEGER)');
-            tx.executeSql('CREATE TABLE IF NOT EXISTS WEDNESDAY (id INTEGER PRIMARY KEY AUTOINCREMENT,exercise TEXT NOT NULL, added INTEGER)');
-            tx.executeSql('CREATE TABLE IF NOT EXISTS THURSDAY (id INTEGER PRIMARY KEY AUTOINCREMENT,exercise TEXT NOT NULL, added INTEGER)');
-            tx.executeSql('CREATE TABLE IF NOT EXISTS FRIDAY (id INTEGER PRIMARY KEY AUTOINCREMENT,exercise TEXT NOT NULL, added INTEGER)');
-            tx.executeSql('CREATE TABLE IF NOT EXISTS SATURDAY (id INTEGER PRIMARY KEY AUTOINCREMENT,exercise TEXT NOT NULL, added INTEGER)');
-            tx.executeSql('CREATE TABLE IF NOT EXISTS SUNDAY (id INTEGER PRIMARY KEY AUTOINCREMENT,exercise TEXT NOT NULL, added INTEGER)');
+            tx.executeSql('CREATE TABLE IF NOT EXISTS MONDAY (id INTEGER PRIMARY KEY AUTOINCREMENT, exercise TEXT NOT NULL, sets INTEGER, reps INTEGER, timer INTEGER, added INTEGER)');
+            tx.executeSql('CREATE TABLE IF NOT EXISTS TUESDAY (id INTEGER PRIMARY KEY AUTOINCREMENT,exercise TEXT NOT NULL, sets INTEGER, reps INTEGER, timer INTEGER, added INTEGER)');
+            tx.executeSql('CREATE TABLE IF NOT EXISTS WEDNESDAY (id INTEGER PRIMARY KEY AUTOINCREMENT,exercise TEXT NOT NULL, sets INTEGER, reps INTEGER, timer INTEGER, added INTEGER)');
+            tx.executeSql('CREATE TABLE IF NOT EXISTS THURSDAY (id INTEGER PRIMARY KEY AUTOINCREMENT,exercise TEXT NOT NULL, sets INTEGER, reps INTEGER, timer INTEGER, added INTEGER)');
+            tx.executeSql('CREATE TABLE IF NOT EXISTS FRIDAY (id INTEGER PRIMARY KEY AUTOINCREMENT,exercise TEXT NOT NULL, sets INTEGER, reps INTEGER, timer INTEGER, added INTEGER)');
+            tx.executeSql('CREATE TABLE IF NOT EXISTS SATURDAY (id INTEGER PRIMARY KEY AUTOINCREMENT,exercise TEXT NOT NULL, sets INTEGER, reps INTEGER, timer INTEGER, added INTEGER)');
+            tx.executeSql('CREATE TABLE IF NOT EXISTS SUNDAY (id INTEGER PRIMARY KEY AUTOINCREMENT,exercise TEXT NOT NULL, sets INTEGER, reps INTEGER, timer INTEGER, added INTEGER)');
         }
 
         // Query the database
@@ -27,7 +36,7 @@
         
         function queryDay(tx) {
             if (day == 'Monday') {
-                tx.executeSql("SELECT * FROM MONDAY", [], queryDaySuccess, errorCB);
+                tx.executeSql("SELECT * FROM MONDAY", [], queryDaySuccess);
             }else if (day == 'Tuesday') {
                 tx.executeSql("SELECT * FROM TUESDAY", [], queryDaySuccess, errorCB);
             }else if (day == 'Wednesday') {
@@ -48,10 +57,122 @@
                     [], querySuccess, errorCB);
         }
         
+        function queryWorkoutMonday(tx) {
+            tx.executeSql("SELECT * FROM MONDAY WHERE ADDED = 1", [], queryWorkoutSuccessMonday);
+        }
+        function queryWorkoutTuesday(tx) {
+            tx.executeSql("SELECT * FROM TUESDAY", [], queryWorkoutSuccessTuesday, errorCB);
+        }
+        function queryWorkoutWednesday(tx) {
+            tx.executeSql("SELECT * FROM WEDNESDAY", [], queryWorkoutSuccessWednesday, errorCB);
+        }
+        function queryWorkoutThursday(tx) {
+            tx.executeSql("SELECT * FROM THURSDAY", [], queryWorkoutSuccessThursday, errorCB);
+        }
+        function queryWorkoutFriday(tx) {
+            tx.executeSql("SELECT * FROM FRIDAY", [], queryWorkoutSuccessFriday, errorCB);
+        }
+        function queryWorkoutSaturday(tx) {
+            tx.executeSql("SELECT * FROM SATURDAY", [], queryWorkoutSuccessSaturday, errorCB);
+        }
+        function queryWorkoutSunday(tx) {
+            tx.executeSql("SELECT * FROM SUNDAY", [], queryWorkoutSuccessSunday, errorCB);
+        }
         
+        
+        function queryWorkoutSuccessMonday(tx, results){
+            var count = document.getElementById("mondayCount").value -1;
+            var id = results.rows.item(count).id;
+            document.getElementById("monday").innerHTML = monday;
+            document.getElementById("mondayWorkout").innerHTML = results.rows.item(count).exercise;
+            document.getElementById("zGoal").innerHTML = results.rows.item(count).sets;
+            document.getElementById("repsGoal").innerHTML = results.rows.item(count).reps;
+            document.getElementById("timeGoal").innerHTML = results.rows.item(count).timer;
+        }
+        function queryWorkoutSuccessTuesday(tx, results){
+            var count = document.getElementById("tuesdayCount").value -1;
+            var id = results.rows.item(count).id;
+            document.getElementById("tuesday").innerHTML = tuesday;
+            document.getElementById("tuesdayWorkout").innerHTML = results.rows.item(count).exercise;
+            document.getElementById("zGoal").innerHTML = results.rows.item(count).sets;
+            document.getElementById("repsGoal").innerHTML = results.rows.item(count).reps;
+            document.getElementById("timeGoal").innerHTML = results.rows.item(count).timer;
+        }
+        function queryWorkoutSuccessWednesday(tx, results){
+            var count = document.getElementById("wednesdayCount").value -1;
+            var id = results.rows.item(count).id;
+            document.getElementById("wednesday").innerHTML = tuesday;
+            document.getElementById("wednesdayWorkout").innerHTML = results.rows.item(count).exercise;
+            document.getElementById("zGoal").innerHTML = results.rows.item(count).sets;
+            document.getElementById("repsGoal").innerHTML = results.rows.item(count).reps;
+            document.getElementById("timeGoal").innerHTML = results.rows.item(count).timer;
+        }
+        function queryWorkoutSuccessThursday(tx, results){
+            var count = document.getElementById("thursdayCount").value -1;
+            var id = results.rows.item(count).id;
+            document.getElementById("thursday").innerHTML = tuesday;
+            document.getElementById("thursdayWorkout").innerHTML = results.rows.item(count).exercise;
+            document.getElementById("zGoal").innerHTML = results.rows.item(count).sets;
+            document.getElementById("repsGoal").innerHTML = results.rows.item(count).reps;
+            document.getElementById("timeGoal").innerHTML = results.rows.item(count).timer;
+        }
+        function queryWorkoutSuccessFriday(tx, results){
+            var count = document.getElementById("fridayCount").value -1;
+            var id = results.rows.item(count).id;
+            document.getElementById("friday").innerHTML = tuesday;
+            document.getElementById("fridayWorkout").innerHTML = results.rows.item(count).exercise;
+            document.getElementById("zGoal").innerHTML = results.rows.item(count).sets;
+            document.getElementById("repsGoal").innerHTML = results.rows.item(count).reps;
+            document.getElementById("timeGoal").innerHTML = results.rows.item(count).timer;
+        }
+        function queryWorkoutSuccessSaturday(tx, results){
+            var count = document.getElementById("saturdayCount").value -1;
+            var id = results.rows.item(count).id;
+            document.getElementById("saturday").innerHTML = tuesday;
+            document.getElementById("saturdayWorkout").innerHTML = results.rows.item(count).exercise;
+            document.getElementById("zGoal").innerHTML = results.rows.item(count).sets;
+            document.getElementById("repsGoal").innerHTML = results.rows.item(count).reps;
+            document.getElementById("timeGoal").innerHTML = results.rows.item(count).timer;
+        }
+        function queryWorkoutSuccessSunday(tx, results){
+            var count = document.getElementById("sundayCount").value -1;
+            var id = results.rows.item(count).id;
+            document.getElementById("sunday").innerHTML = tuesday;
+            document.getElementById("sundayWorkout").innerHTML = results.rows.item(count).exercise;
+            document.getElementById("zGoal").innerHTML = results.rows.item(count).sets;
+            document.getElementById("repsGoal").innerHTML = results.rows.item(count).reps;
+            document.getElementById("timeGoal").innerHTML = results.rows.item(count).timer;
+        }
+        function mondayNext() {
+            document.getElementById("mondayCount").stepUp(1);
+            loadWorkoutMonday();
+        }
+        function tuesdayNext() {
+            document.getElementById("tuesdayCount").stepUp(1);
+            loadWorkoutTuesday();
+        }
+        function wednesdayNext() {
+            document.getElementById("wednesdayCount").stepUp(1);
+            loadWorkoutWednesday();
+        }
+        function thursdayNext() {
+            document.getElementById("thursdayCount").stepUp(1);
+            loadWorkoutThursday();
+        }
+        function fridayNext() {
+            document.getElementById("fridayCount").stepUp(1);
+            loadWorkoutFriday();
+        }
+        function saturdayNext() {
+            document.getElementById("saturdayCount").stepUp(1);
+            loadWorkoutSaturday();
+        }
+        function sundayNext() {
+            document.getElementById("sundayCount").stepUp(1);
+            loadWorkoutSunday();
+        }
         
         // Query the success callback
-        //
         function querySuccess(tx, results) {
             var tblText='<table id="t01"><tr><th id= "idPad">ID </th> <th id= "namePad"> Exercise name </th> <th id= "repsPad">Reps </th> <th id= "setsPad"> Sets </th>  <th id= "timePad"> Timers </th>  <th id= "incPad"> increment </th></tr>';
             var len = results.rows.length;
@@ -74,6 +195,7 @@
             successCBday();
             //document.getElementById("dayDiv").innerHTML = value;
         }
+        
         function queryDaySuccess(tx,results){
             var tblText = '<table><tr><th>Name </th> <th> Added </th></tr>' + day;
             var len = results.rows.length;
@@ -128,6 +250,34 @@
             db.transaction(queryDay, errorCB);
         }
         
+        function loadWorkoutMonday() {
+            var db = sqlitePlugin.openDatabase("Database", "1.0", "Cordova Demo", 200000);
+            db.transaction(queryWorkoutMonday, errorCB);
+        }
+        function loadWorkoutTuesday() {
+            var db = sqlitePlugin.openDatabase("Database", "1.0", "Cordova Demo", 200000);
+            db.transaction(queryWorkoutTuesday, errorCB);
+        }
+        function loadWorkoutWednesday() {
+            var db = sqlitePlugin.openDatabase("Database", "1.0", "Cordova Demo", 200000);
+            db.transaction(queryWorkoutWednesday, errorCB);
+        }
+        function loadWorkoutThursday() {
+            var db = sqlitePlugin.openDatabase("Database", "1.0", "Cordova Demo", 200000);
+            db.transaction(queryWorkoutThursday, errorCB);
+        }
+        function loadWorkoutFriday() {
+            var db = sqlitePlugin.openDatabase("Database", "1.0", "Cordova Demo", 200000);
+            db.transaction(queryWorkoutFriday, errorCB);
+        }
+        function loadWorkoutSaturday() {
+            var db = sqlitePlugin.openDatabase("Database", "1.0", "Cordova Demo", 200000);
+            db.transaction(queryWorkoutSaturday, errorCB);
+        }
+        function loadWorkoutSunday() {
+            var db = sqlitePlugin.openDatabase("Database", "1.0", "Cordova Demo", 200000);
+            db.transaction(queryWorkoutSunday, errorCB);
+        }
         function successCB() {
             var db = sqlitePlugin.openDatabase("Database", "1.0", "Cordova Demo", 200000);
             db.transaction(queryDB, errorCB);
@@ -149,13 +299,41 @@
                     +'","'+document.getElementById("time").value
                     +'","'+document.getElementById("increment_rate").value
                     + '")');
-            tx.executeSql('INSERT INTO MONDAY (exercise,added) VALUES ("' + document.getElementById("exercise_name").value + '","0")');
-            tx.executeSql('INSERT INTO TUESDAY (exercise,added) VALUES ("' + document.getElementById("exercise_name").value + '","0")');
-            tx.executeSql('INSERT INTO WEDNESDAY (exercise,added) VALUES ("' + document.getElementById("exercise_name").value + '","0")');
-            tx.executeSql('INSERT INTO THURSDAY (exercise,added) VALUES ("' + document.getElementById("exercise_name").value + '","0")');
-            tx.executeSql('INSERT INTO FRIDAY (exercise,added) VALUES ("' + document.getElementById("exercise_name").value + '","0")');
-            tx.executeSql('INSERT INTO SATURDAY (exercise,added) VALUES ("' + document.getElementById("exercise_name").value + '","0")');
-            tx.executeSql('INSERT INTO SUNDAY (exercise,added) VALUES ("' + document.getElementById("exercise_name").value + '","0")');
+            tx.executeSql('INSERT INTO MONDAY (exercise,sets,reps,timer,added) VALUES ("' + document.getElementById("exercise_name").value
+                          +'","' + document.getElementById("sets_number").value
+                          +'","' + document.getElementById("reps_number").value
+                          +'","' + document.getElementById("time").value
+                          + '","0")');
+            tx.executeSql('INSERT INTO TUESDAY (exercise,sets,reps,timer,added) VALUES ("' + document.getElementById("exercise_name").value
+                          +'","' + document.getElementById("sets_number").value
+                          +'","' + document.getElementById("reps_number").value
+                          +'","' + document.getElementById("time").value
+                          + '","0")');
+            tx.executeSql('INSERT INTO WEDNESDAY (exercise,sets,reps,timer,added) VALUES ("' + document.getElementById("exercise_name").value
+                          +'","' + document.getElementById("sets_number").value
+                          +'","' + document.getElementById("reps_number").value
+                          +'","' + document.getElementById("time").value
+                          + '","0")');
+            tx.executeSql('INSERT INTO THURSDAY (exercise,sets,reps,timer,added) VALUES ("' + document.getElementById("exercise_name").value
+                          +'","' + document.getElementById("sets_number").value
+                          +'","' + document.getElementById("reps_number").value
+                          +'","' + document.getElementById("time").value
+                          + '","0")');
+            tx.executeSql('INSERT INTO FRIDAY (exercise,sets,reps,timer,added) VALUES ("' + document.getElementById("exercise_name").value
+                          +'","' + document.getElementById("sets_number").value
+                          +'","' + document.getElementById("reps_number").value
+                          +'","' + document.getElementById("time").value
+                          + '","0")');
+            tx.executeSql('INSERT INTO SATURDAY (exercise,sets,reps,timer,added) VALUES ("' + document.getElementById("exercise_name").value
+                          +'","' + document.getElementById("sets_number").value
+                          +'","' + document.getElementById("reps_number").value
+                          +'","' + document.getElementById("time").value
+                          + '","0")');
+            tx.executeSql('INSERT INTO SUNDAY (exercise,sets,reps,timer,added) VALUES ("' + document.getElementById("exercise_name").value
+                          +'","' + document.getElementById("sets_number").value
+                          +'","' + document.getElementById("reps_number").value
+                          +'","' + document.getElementById("time").value
+                          + '","0")');
             document.getElementById("exercise_name").value = "";
             document.getElementById("reps_number").value = "";
             document.getElementById("sets_number").value = "";
